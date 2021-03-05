fa823ac61336da4d5b9dc8c185422c577fe05842
obedabit <obitbit30@gmail.com>
2021-03-05 20:19:16 +0200

    Create Basic Components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import './App.css';
+import React from "react";
+import NavBar from "./components/Navbar";
+import IFrame from './components/IFrame';
+import LeftSideMenu from './components/LeftSideMenu';
+import RightSideMenu from './components/RightSideMenu';
 
-function App() {
-  return (
-    <div>
-      <h1>Web Builder</h1>
-      <div>
-        <iframe src='target.html'></iframe>
-      </div>
-    </div>
-  );
+import "./App.css";
+
+export default function App() {
+	const drop = e => {
+		e.preventDefault();
+
+		const card_id = e.dataTransfer.getData("card_id");
+		const card = document.getElementById(card_id);
+		
+		e.target.appendChild(card);
+	};
+
+	const dragOver = e => {
+		e.preventDefault();
+	};
+
+	const dragStart = e => {
+		const target = e.target;
+		e.dataTransfer.setData("card_id", target.id);
+
+		setTimeout(() => {
+			target.style.display = "none";
+		}, 0);
+	};
+	const dragOverC = e => {
+		e.stopPropagation();
+	};
+
+	return (
+		<div className="app">
+			<NavBar />
+			<IFrame />
+			<LeftSideMenu />
+			<RightSideMenu />
+		</div>
+	);
 }
-
-export default App;
